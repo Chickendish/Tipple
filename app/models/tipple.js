20804664c70b05e5ac8af79c045097309692d34d
wcwhite71 <william.c.white4@gmail.com>
2017-08-23 16:52:22 -0600

    insertseeds

--- app/models/tipple.js
+++ app/models/tipple.js
@@ -4,7 +4,7 @@ var Sequelize = require("sequelize");
 // Require the connection to the database (connection.js)
 var sequelize = require("../config/connection.js");
 
-// Create a cocktails table on tipple database 
+// Create a cocktails table on tipple database
 
 var tipple = sequelize.define("cocktail", {
   id:{
@@ -42,6 +42,27 @@ var tipple = sequelize.define("cocktail", {
   timestamps: true
 });
 // Sync
-tipple.sync();
+tipple.sync({
+  force: true
+}).then(function() {
+  tipple.create({
+    name: "Whiskey Sour",
+    ingredient1: "Whiskey",
+    ingredient2: "grated ginger",
+    ingredient3: "",
+    mixer: "Ginger Ale",
+    garnish: "",
+    measure: "0.25"
+  });
+  tipple.create({
+    name: "Whiskey Sour",
+    ingredient1: "Whiskey",
+    ingredient2: "grated ginger",
+    ingredient3: "",
+    mixer: "Ginger Ale",
+    garnish: "",
+    measure: "0.25"
+  });
+});
 // Export
 module.exports = tipple;
