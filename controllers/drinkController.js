73debfe2a187a4afb6d6022e9d4b458e13741710
Bill Donovan <williamcdonovan@gmail.com>
2017-08-23 14:23:23 -0600

    everything is broken

---
+++ controllers/drinkController.js
@@ -0,0 +1,8 @@
+// var express = require("express");
+
+// var router = express.Router();
+
+// // Import the model (cat.js) to use its database functions.
+// var Drinks = require("../models/drink.js");
+
+// Create all our routes and set up logic within those routes where required.
