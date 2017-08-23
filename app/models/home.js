4f2cb15946c46686aadc638158a4f454614b92aa
wcwhite71 <william.c.white4@gmail.com>
2017-08-23 16:12:58 -0600

    add name category

--- app/models/home.js
+++ app/models/home.js
@@ -3,40 +3,42 @@
 var Sequelize = require("sequelize");
 // Require the connection to the database (connection.js)
 var sequelize = require("../config/connection.js");
-// Create a recipe table on homemade database 
+// Create a recipe table on homemade database
 var tipple = sequelize.define("cocktail", {
   id:{
     type:Sequelize.INTEGER,
     autoIncrement:true,
     primaryKey:true
   },
-
+  name: {
+    type.Sequelize.STRING,
+  },
   ingredient1: {
    type:Sequelize.STRING
-  }, 
-  ingredient2: { 
-    type:Sequelize.STRING
-  }, 
-
-  ingredient3: { 
+  },
+  ingredient2: {
     type:Sequelize.STRING
   },
 
-  mixer: { 
-    type:Sequelize.STRING
-  }, 
-  garnish: { 
+  ingredient3: {
     type:Sequelize.STRING
   },
-  measure: { 
+
+  mixer: {
+    type:Sequelize.STRING
+  },
+  garnish: {
+    type:Sequelize.STRING
+  },
+  measure: {
     type:Sequelize.INTEGER
 }
 
-}, 
+},
 {
   timestamps: true
 });
-// Sync 
+// Sync
 tipple.sync();
-// Export 
+// Export
 module.exports = tipple;
