065d7d45507f5b4f7c83ed0a2d98bd03b2ea085c
wcwhite71 <william.c.white4@gmail.com>
2017-08-25 00:19:42 -0600

    ajax call for api

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -1,16 +1,13 @@
 
-$("#add-btn").on("click", function(event) {
-  event.preventDefault();
-  var cocktail = $("#cocktail-search").val().trim();
-  // Make an AJAX get request to our api, including the user's book in the url
-  $.get("/api/" + cocktailSearched, function(data) {
-    console.log(data);
-    // Call our renderBooks function to add our books to the page
-    render tipple(data);
-
-  });
-
-});
-
-
-
+// $("#add-btn").on("click", function(event) {
+//   event.preventDefault();
+//   var cocktail = $("#cocktail-search").val().trim();
+//   // Make an AJAX get request to our api, including the user's book in the url
+//   $.get("/api/" + cocktailSearched, function(data) {
+//     console.log(data);
+//     // Call our renderBooks function to add our books to the page
+//     render tipple(data);
+//
+//   });
+//
+// });
