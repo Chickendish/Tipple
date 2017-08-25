061a3551aa4a3329b12a743d18b2cb7f3e73edc4
Bill Donovan <williamcdonovan@gmail.com>
2017-08-24 21:24:38 -0600

    corrected missing bracket

--- app/models/tipple.js
+++ app/models/tipple.js
@@ -5,60 +5,99 @@ var Sequelize = require("sequelize");
 var sequelize = require("../config/connection.js");
 // Create a cocktails table on tipple database
 var tipple = sequelize.define("cocktail", {
+
   id:{
     type:Sequelize.INTEGER,
     autoIncrement:true,
     primaryKey:true
   },
 
-  name: {
+  strDrink: {
     type:Sequelize.STRING,
+    validate: {
+      notNull: true,
+      isAlphanumeric: true
+    }
   },
-  ingredient1: {
+
+  strInstructions: {
+    type:Sequelize.TEXT
+  },
+
+  strIngredient1: {
    type:Sequelize.STRING
   },
-  ingredient2: {
+
+  strIngredient2: {
     type:Sequelize.STRING
   },
 
-  ingredient3: {
+  strIngredient3: {
     type:Sequelize.STRING
   },
 
-  mixer: {
+  strIngredient4: {
     type:Sequelize.STRING
   },
-  garnish: {
+
+  strIngredient5: {
     type:Sequelize.STRING
   },
-  measure: {
+
+  strIngredient6: {
+    type:Sequelize.STRING
+  },
+
+  strMeasure1: {
     type:Sequelize.INTEGER
-},
+  },
 
-{
+  strMeasure2: {
+    type:Sequelize.INTEGER
+  },
+
+  strMeasure3: {
+    type:Sequelize.INTEGER
+  },
+
+  strMeasure4: {
+    type:Sequelize.INTEGER
+  },
+
+  strMeasure5: {
+    type:Sequelize.INTEGER
+  },
+
+  strMeasure6: {
+    type:Sequelize.INTEGER
+  },
+
+  {
   timestamps: true
+  }
 });
+
 // Sync
 tipple.sync({
   force: true
 }).then(function() {
   tipple.create({
-    name: "Whiskey Sour",
-    ingredient1: "Whiskey",
-    ingredient2: "grated ginger",
-    ingredient3: "",
-    mixer: "Ginger Ale",
-    garnish: "",
-    measure: "0.25"
+    strDrink: "Whiskey Sour",
+    strIngredient1: "Whiskey",
+    strIngredient2: "grated ginger",
+    strIngredient3: "",
+    strIngredient4: "Ginger Ale",
+    strIngredient5: "",
+    strMeasure1: "0.25"
   });
   tipple.create({
-    name: "Whiskey Sour",
-    ingredient1: "Whiskey",
-    ingredient2: "grated ginger",
-    ingredient3: "",
-    mixer: "Ginger Ale",
-    garnish: "",
-    measure: "0.25"
+    strDrink: "Whiskey Sour",
+    strIngredient1: "Whiskey",
+    strIngredient2: "grated ginger",
+    strIngredient3: "",
+    strIngredient4: "Ginger Ale",
+    strIngredient5: "",
+    strMeasure1: "0.25"
   });
 });
 // Export
