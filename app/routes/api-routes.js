08f5b1c1fda00672f3b24859f6ba083754e02c9c
Bill Donovan <williamcdonovan@gmail.com>
2017-08-26 12:25:20 -0600

    added handlebars views

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,3 +1,4 @@
+var request = require('request');
 
 var tipple = require("../app/models/tipple.js");
 module.exports = function(app){
@@ -6,6 +7,15 @@ module.exports = function(app){
       res.json(results);
     });
   });
+	app.get('/api/drinks/ingredients/:ingredient', (req, res)=>{
+		request('http://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + req.params.ingredient, function (error, response, body) {
+  			console.log('error:', error); // Print the error if one occurred 
+  			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  			console.log('body:', body); // Print the HTML for the Google homepage. 
+  			res.json(body);
+		});
+
+	});
 }
 
 
