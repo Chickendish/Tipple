14152c438e4efb47db70baec86f337e4eac21102
Bill Donovan <williamcdonovan@gmail.com>
2017-08-27 11:04:21 -0600

    Change is the removal of a blank line
    Deletion of a blank line.

--- app/controllers/drinkController.js
+++ app/controllers/drinkController.js
@@ -1,6 +1,6 @@
 //get express
 var express = require("express");
-
+//get router
 var router = express.Router();
 // Import the model (cat.js) to use its database functions.
 var Drinks = require("../models/drink.js");
